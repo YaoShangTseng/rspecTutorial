9d455ea97d9effb48cbf76a2a7d729763fb53409
YaoShangTseng <yakushou730@gmail.com>
2016-08-10 16:56:38 +0800

    model rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  has_many :comments
+
+  def self.no_content
+    where(content: nil)
+  end
+
+  def abstract
+    self.content[0..20]
+  end
+
 end
